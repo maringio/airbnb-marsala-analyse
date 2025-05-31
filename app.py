a8371dc2078c28a647e3dc1f5bd6e4d75cc75fb8
maringio <giovanna.marino@bluewin.ch>
2025-05-31 23:20:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Airbnb Preisanalyse – Marsala 🇮🇹")
+st.title("🏠 Prezzi Airbnb a Marsala – Dati, tendenze e osservazioni")
+
+st.markdown("""
+WBenvenuti all’analisi dei dati degli alloggi Airbnb a Marsala 🇮🇹.
+Qui troverai una panoramica iniziale sul numero e le caratteristiche degli annunci su Airbnb.
+""")
 
 df = pd.read_csv("listings.csv")
 st.write("Anzahl Einträge:", len(df))
-
 st.dataframe(df.head())
