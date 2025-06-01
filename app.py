c0135cdf66348c970f87677ef1cbf54bb618ab7d
maringio <giovanna.marino@bluewin.ch>
2025-06-01 12:07:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ import pandas as pd
 st.title("🏠 Prezzi Airbnb a Marsala – Dati, tendenze e osservazioni")
 
 st.markdown("""
-WBenvenuti all’analisi dei dati degli alloggi Airbnb a Marsala 🇮🇹.  
+Benvenuti all’analisi dei dati degli alloggi Airbnb a Marsala 🇮🇹.  
 Qui troverai una panoramica iniziale sul numero e le caratteristiche degli annunci su Airbnb a Marsala.
 """)
 
 # CSV einlesen und Vorschau anzeigen
 df = pd.read_csv("listings.csv")
-st.write("Anzahl Einträge:", len(df))
+st.write("Numero di elementi:", len(df))
 st.dataframe(df.head())
 
 # Bild über GitHub-Link einbinden
-st.subheader("📊 Preisentwicklung pro Monat")
-st.markdown("Die Preise für eine Übernachtung in Marsala sind erstaunlich konstant – es zeigt sich keine starke Saisonalität.")
+st.subheader("📊 Sviluppo prezzi al mese")
+st.markdown("""
+I prezzi per un pernottamento a Marsala sono relativamente costanti. Non c'è una stagionalità forte.
+l prezzo medio su Airbnb per notte a Marsala è di circa **88 Euro**.
+""")
 
 st.image(
     "https://raw.githubusercontent.com/maringio/airbnb-marsala-analyse/main/preise_pro_monat_bereinigt.png",
